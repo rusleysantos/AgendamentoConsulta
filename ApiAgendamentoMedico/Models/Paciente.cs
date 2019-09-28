7d12cbb47d3d52d1f68416d4b8ac70cc06a4f9d1
rusleysantos <rusley_silva@hotmail.com>
2019-09-28 16:13:37 -0300

    Adiciona contexto do EF no projeto

---
+++ ApiAgendamentoMedico/Models/Paciente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApiAgendamentoMedico.Models
+{
+    public partial class Paciente
+    {
+        public Paciente()
+        {
+            Consulta = new HashSet<Consulta>();
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Nascimento { get; set; }
+
+        public ICollection<Consulta> Consulta { get; set; }
+    }
+}
