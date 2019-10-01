509025f306de4eed3315bf59cdf50ffcb274f3a9
rusley.santos <rusley.santos@E5AT52>
2019-10-01 17:07:31 -0300

    Adiciona nova teste unitario teste data set consulta

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -31,5 +31,22 @@ namespace UnitTestProject1
             );
 
         }
+
+        /// <summary>
+        /// Verifica se o sistema está deixando inserir hora de inicio inferior a data de termino
+        /// </summary>
+        [TestMethod]
+        public void TesteDataSetConsulta()
+        {
+            GerenciarConsultaController consulta = new GerenciarConsultaController();
+
+            if (consulta.setConsulta("10-10-2019", "Teste", "11:00", "10:00", "Tese"))
+            {
+                throw new Exception("Hora de inicio de consulta menor que hora de termino");
+            }
+
+        }
+
+
     }
 }
