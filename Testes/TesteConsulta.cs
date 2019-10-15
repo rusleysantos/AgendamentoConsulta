f8cb0c2f945925a44c9b594677180a7712f1e044
rusley.santos <rusley.santos@E5AT52>
2019-10-15 10:05:57 -0300

    Adiciona testes

--- Testes/TesteConsulta.cs
+++ Testes/TesteConsulta.cs
@@ -1,13 +1,25 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Servicos.GerenciarConsulta;
+using System;
 
 namespace Testes
 {
     [TestClass]
     public class TesteConsulta
     {
+        /// <summary>
+        /// Verifica se o sistema está deixando inserir hora de inicio inferior a data de termino
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void TesteDataSetConsulta()
         {
+            InserirConsulta consulta = new InserirConsulta();
+
+            if (consulta.Inserir("10-10-2019", "Teste", "11:00", "10:00", "Tese"))
+            {
+                throw new Exception("Hora de inicio de consulta menor que hora de termino");
+            }
+
         }
     }
 }
